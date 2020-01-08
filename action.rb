f13ce32fcfe4d8dc017205ce1f3bb2c68c093750
Joao Duarte <jsvduarte@gmail.com>
2020-01-08 16:34:48 +0000

    better debugging information

--- action.rb
+++ action.rb
@@ -13,6 +13,11 @@ puts ENV.inspect
 puts "\nlisting files"
 puts `ls`
 =end
+BASE_REF = ENV['GITHUB_BASE_REF']
+unless BASE_REF
+  $stderr.puts "ERROR: GITHUB_BASE_REF environment variable not found. Aborting.."
+  exit(1)
+end
 def find_pr_version
   gemspec_path = Dir.glob("*.gemspec").first
   spec = Gem::Specification::load(gemspec_path)
@@ -28,26 +33,30 @@ def find_version_changelog_entry(version)
 end
 
 def compute_changelog_suggestion
-  commits = `git log master.. --format=%B`.split("\n")
-  commits.map {|commit| "  - #{commit}" }
+  commits = `git log #{BASE_REF}.. --format=%B`.split("\n")
+  if commits.empty?
+    "  - could not find commits between this Pull Request and the \"#{BASE_REF}\" branch."
+  else
+    commits.map {|commit| "  - #{commit}" }
+  end
 end
 
 pr_version = find_pr_version()
-puts "Plugin version is this PR is: #{pr_version}"
+puts "Plugin version in this PR is: #{pr_version}"
 
 published_versions = fetch_git_versions()
 
 if published_versions.include?(pr_version)
-  puts "Version #{pr_version} has already been published to Rubygems.org"
-  puts "Please bump the version to speed up plugin publishing after this PR is merged"
+  $stderr.puts "ERROR: Version #{pr_version} has already been published to Rubygems.org"
+  $stderr.puts "ERROR: Please bump the version to speed up plugin publishing after this PR is merged"
   exit(1)
 end
 
 unless match = find_version_changelog_entry(pr_version)
-  puts "We were unable to find a CHANGELOG.md entry for version #{pr_version}"
-  puts "Please at a new entry to the top of CHANGELOG.md similar to:\n"
-  puts "## #{pr_version}"
-  puts compute_changelog_suggestion()
+  $stderr.puts "ERROR: We were unable to find a CHANGELOG.md entry for version #{pr_version}"
+  $stderr.puts "Please add a new entry to the top of CHANGELOG.md similar to:\n\n"
+  $stderr.puts "## #{pr_version}"
+  $stderr.puts compute_changelog_suggestion()
   exit(1)
 else
   puts "Found changelog entry for version #{pr_version}:"
