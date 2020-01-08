f0c6b4f03a1a86845eb63b1e064209ab9d7ce3f4
Joao Duarte <jsvduarte@gmail.com>
2020-01-08 16:13:42 +0000

    find changelog entry

--- action.rb
+++ action.rb
@@ -2,6 +2,7 @@ require "json"
 require "rubygems"
 
 # Each Action has an event passed to it.
+=begin
 event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
 puts "event inspect:"
 puts event.inspect
@@ -11,16 +12,25 @@ puts ENV.inspect
 
 puts "\nlisting files"
 puts `ls`
+=end
 def find_pr_version
   gemspec_path = Dir.glob("*.gemspec").first
   spec = Gem::Specification::load(gemspec_path)
   spec.version
 end
 
+def fetch_git_versions
+  `git tag -l`.split("\n").map {|version| version.tr("v", "") }
+end
+
+def find_version_changelog_entry(version)
+  IO.read("CHANGELOG.md").match(/## #{version}\n\s+.+$/)
+end
+
 pr_version = find_pr_version()
 puts "Plugin version is this PR is: #{pr_version}"
 
-published_versions = `git tag -l`.split("\n").map {|version| version.tr("v", "") }
+published_versions = fetch_git_versions()
 
 if published_versions.include?(pr_version)
   puts "This version has already been published to Rubygems.org"
@@ -28,10 +38,13 @@ if published_versions.include?(pr_version)
   exit(1)
 end
 
-if IO.read("CHANGELOG.md").match(/## #{pr_version}\n\s+.+$/)
+unless match = find_version_changelog_entry(pr_version)
   puts "We were unable to find a CHANGELOG.md entry for version #{pr_version}"
   puts "Please at a new entry to the top of CHANGELOG.md similar to:\n"
   puts "## #{pr_version}"
   puts "  - text"
   exit(1)
+else
+  puts "found entry for version #{pr_version}"
+  puts match.inspect
 end
